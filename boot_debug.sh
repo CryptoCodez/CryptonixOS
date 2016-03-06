6b1f9472cc24c402cf068d3ee749bf8fa273eac5
CryptoCodez <CryptoCodez@protonmail.com>
2016-03-06 23:16:42 +0100

    modified:   boot_debug.sh

--- boot_debug.sh
+++ boot_debug.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -S -s -singlestep -kernel kernel -serial file:serial.log & uxterm -e "gdb -tui -iex \"target remote 127.0.0.1:1234\" -iex \"symbol-file kernel\""
+qemu-system-i386 -S -s -singlestep -kernel kernel -serial file:serial.log & uxterm -e "gdb -iex \"target remote 127.0.0.1:1234\" -iex \"symbol-file kernel\""
