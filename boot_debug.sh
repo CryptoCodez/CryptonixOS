95cf31c23398a439b7570d684c2d44eb30430abb
cryptocodez <cryptocodez@protonmail.com>
2016-03-26 05:21:14 +0100

    local_update

--- boot_debug.sh
+++ boot_debug.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -S -s -singlestep -kernel kernel & uxterm -e "gdb -iex \"target remote 127.0.0.1:1234\" -iex \"symbol-file kernel\""
+qemu-system-i386 -S -s -singlestep -kernel kernel & xfce4-terminal -e "gdb -iex \"target remote 127.0.0.1:1234\" -iex \"symbol-file kernel\""
