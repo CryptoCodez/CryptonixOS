b653093e90e3d1d0e229563d5f575f9b111ab53d
cryptocodez <cryptocodez@protonmail.com>
2016-03-26 11:30:12 +0100

    memory_iterator

--- memory.hpp
+++ memory.hpp
@@ -4,10 +4,35 @@
 #ifndef _MEMORY_HPP_
 #define _MEMORY_HPP_
 
-#include "types.hpp"
-
 namespace mem {
 
+template <typename Iterator, unsigned long long int N>
+class iterator {
+public:
+  // lifecycle
+  //
+  iterator( Iterator* pos ) : begin_( pos ), pos_( begin_ ), 
+    end_( begin_ + sizeof( Iterator ) * N ) {}
+  iterator( const iterator& iter ) : begin_( iter.begin_ ),
+    pos_( iter.pos_ ), end_( iter.end_ ) {}
+  
+  ~iterator() {}
+
+  // operator overloading
+  //
+  iterator& operator++() { ++pos; return *this; }
+  iterator operator++( int ) { 
+    iterator tmp( *this ); operator++(); return tmp;
+  }
+  bool operator==( const iterator& rval ) { return pos_==rval.pos_; }
+  bool operator!=( const iterator& rval ) { return pos_!=rval.pos_; }
+  int& operator*() { return *pos_ }
+
+private:
+  Iterator* begin_;
+  Iterator* pos_;
+  Iterator* end_;
+};
 
 } // namespace mem
 
